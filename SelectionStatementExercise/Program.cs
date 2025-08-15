8284a3e2f7a578478685153f679c9469754882eb
Theo Boateng <Theoboateng2002@gmail.com>
2025-08-15 16:11:58 -0400

    initial commit

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,57 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            int favNumber = r.Next(1, 1000);
+            Console.WriteLine("Try to guess my favorite number");
+
+            int userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("Too low!");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("Too high!");
+            }
+            else
+            {
+                Console.WriteLine("Nevermind, you guessed it!");
+            }
+            Console.WriteLine("What's your favorite school subject?");
+            string subject = Console.ReadLine().ToLower();
+
+            switch (subject)
+            {
+                case "math":
+                    Console.WriteLine("Math - the language of the universe!");
+                    break;
+                case"science":
+                    Console.WriteLine("Science - where curiosity meets discovery!");
+                    break;
+                case "history":
+                    Console.WriteLine("History — the best way to avoid past mistakes.");
+                    break;
+                case "english":
+                    Console.WriteLine("English — unlocking the power of words.");
+                    break;
+                case "art":
+                    Console.WriteLine("Art — creativity in its purest form.");
+                    break;
+                default:
+                    Console.WriteLine("Interesting choice! That’s a great subject too.");
+                    break;
+                    
+            }
+            
+                
+
+
+
+
+
+
         }
     }
 }
